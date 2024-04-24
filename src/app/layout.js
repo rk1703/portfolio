25f2ed3e318d041c57a44df8cb9ff79d24e67666
Ravikant <rk1703world@gmail.com>
2024-04-24 11:50:30 +0530

    manual seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,30 @@ import "./globals.css";
 export const metadata = {
   title: "Ravikant | 3D Portfolio",
   description: "My Portfolio website",
+  openGraph: {
+    title: "Ravikant | 3D Portfolio",
+    description: "My Portfolio website",
+    url: "https://rkworld.me",
+    siteName: "Ravikant | 3D Portfolio",
+    images: [
+      {
+        url: "https://www.rkworld.me/seo-cover.png",
+        width: 800,
+        height: 600,
+        alt: "Ravikant | 3D Portfolio",
+      },
+    ],
+    type: "website",
+    locale: "en_IE",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Ravikant | 3D Portfolio",
+    description: "My Portfolio website",
+    creator: "@rkworld",
+    images: "https://www.rkworld.me/seo-cover.png",
+  },
+  category: "technology",
 };
 
 export default function RootLayout({ children }) {
